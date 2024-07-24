13d29b00e254872e336ac60eaece8730faeff081
Javier Razo <javier.razo1221@gmail.com>
2024-07-23 20:15:20 -0600

    Add initial Android Studio project setup

---
+++ java/MyFirstApp/android/src/main/java/com/Javier/myfirstapp/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.Javier.myfirstapp.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.Javier.myfirstapp.MyFirstApp;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new MyFirstApp(), configuration);
+    }
+}
